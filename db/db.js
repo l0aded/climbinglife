76f1ddcadbdc0096c29b122d214d044fdf155439
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-09-20 22:24:40 -0700

    partially added db and backend

--- db/db.js
+++ db/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/test';
+const URI = 'mongodb://localhost/climbinglife';
 var db = mongoose.connection;
 
 //===== DBSchema =======
-var Schema = mongoose.Schema;
+// var Schema = mongoose.Schema;
 
+const cardSchema = mongoose.Schema({
+  listId: { type: Number , unique: true },
+  listTitle: String,
+  createdAt: { type: Date , default: Date.now },
+  items: { type : Array , "default" : [] },
+})
 
+const submitURL = (url) => {
+  console.log('submitted' + url)
 
+}
 
 //======================
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
+const db = require('../db/db.js');
 
 const app = express();
 const PORT = 3000;
 
 app.use(parser.json());
-
 app.use(express.static(path.join(__dirname, '../dist')))
 
 
 //Router for Server
+
+app.get('/submit/:url', (req, res) => {
+  const url = req.params.url;
+  // console.log('server get one list triggered ', req.params.listId)
+  db.findOneList(url)
+  .then((data) => {
+    // console.log('get one list success data: ', data)
+    res.send(data)
+  })
+})
 
 
 app.listen(PORT, () => {
