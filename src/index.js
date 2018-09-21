6099a966e835b70d07d98f118f5b31dc2570f491
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-09-20 17:53:32 -0700

    add grid

--- src/index.js
+++ src/index.js
@@ -8,20 +8,56 @@ require('./css/index.css');
 type Props = {||};
 
 class Index extends React.Component<Props> {
+	constructor(props) {
+		super(props);
+		this.state = {
+			imageURLs: [
+				'https://s3-us-west-1.amazonaws.com/climbinglife/LRG_DSC01229.JPG','https://s3-us-west-1.amazonaws.com/climbinglife/ORG_DSC01009.JPG','https://s3-us-west-1.amazonaws.com/climbinglife/ORG_DSC01212.JPG',
+				'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00971.JPG', 'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00978.JPG', 'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00986.JPG'
+		]
+		}
+	}
 	render() {
 		return (
 			<div className="main">
-				<div className="header">
-					Header
-				</div>
 				<div className="about">
-					Other Stuff
+					<div className="header">
+						<div className="header-left">
+							About Bowser
+						</div>
+						<div className="header-center">
+							Library
+						</div>
+						<div className="header-right">
+							Admin
+						</div>
+					</div>
+					<div className="announcement">
+					</div>
 				</div>
-
+				<Grid
+					urls = {this.state.imageURLs}
+				/>
 			</div>
 		)
 	}
 }
+
+
+const Grid = ( {urls} ) => (
+	<div className="grid-container">
+		{urls.map((url, index) =>
+			<Cell
+				key = {index}
+				url = {url}
+			/>
+		)}
+	</div>
+)
+
+const Cell = ( {url} ) => (
+	<div className="grid-item"><img className="grid-image" src={url}/></div>
+)
 
 const dom = document.getElementById('App')
 if (dom === null) {
