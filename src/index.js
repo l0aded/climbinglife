9ade251839f1d76e54654084c5ecb307cf09cabb
l0aded <oliverhanw@gmail.com>
2019-02-12 09:49:27 -0800

    factory reset

--- src/index.js
+++ src/index.js
@@ -7,61 +7,13 @@ require('./css/index.css');
 
 type Props = {||};
 
-class Index extends React.Component<Props> {
-	constructor(props) {
-		super(props);
-		this.state = {
-			imageURLs: [
-				'https://s3-us-west-1.amazonaws.com/climbinglife/LRG_DSC01229.JPG','https://s3-us-west-1.amazonaws.com/climbinglife/ORG_DSC01009.JPG','https://s3-us-west-1.amazonaws.com/climbinglife/ORG_DSC01212.JPG',
-				'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00971.JPG', 'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00978.JPG', 'https://s3-us-west-1.amazonaws.com/climbinglife/DSC00986.JPG'
-		]
-		}
-	}
-	render() {
-		return (
-			<div className="main">
-				<div className="about">
-					<div className="header">
-						<div className="header-left">
-							About Bowser
-						</div>
-						<div className="header-center">
-							Library
-						</div>
-						<div className="header-right">
-							Admin
-						</div>
-					</div>
-					<div className="announcement">
-					</div>
-				</div>
-				<div className="seperator">
-				</div>
-				<Grid
-					urls = {this.state.imageURLs}
-				/>
-			</div>
-		)
-	}
+function Index() {
+	return (
+		<React.Fragment>
+			Index
+		</React.Fragment>
+	)
 }
-
-
-const Grid = ( {urls} ) => (
-	<div className="grid-container">
-		{urls.map((url, index) =>
-			<Cell
-				key = {index}
-				url = {url}
-			/>
-		)}
-	</div>
-)
-
-const Cell = ( {url} ) => (
-	<div className="grid-item">
-		<img className="grid-image" src={url}/>
-	</div>
-)
 
 const dom = document.getElementById('App')
 if (dom === null) {
